a509424677ed9726453d3395a05963900d1d554d
Imdraks <ayman.hattab01@gmail.com>
2026-04-23 16:16:55 +0200

    chore: ajout fix_ngrok.sh, suppression theme.js inutilise

---
+++ fix_ngrok.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+sed -i 's|ngrok http 80|ngrok http 127.0.0.1:80|' /etc/systemd/system/ngrok.service
+systemctl daemon-reload
+systemctl restart ngrok
+sleep 2
+systemctl status ngrok --no-pager -l
